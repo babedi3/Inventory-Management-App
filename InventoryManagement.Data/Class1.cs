d38c4e714ed9884d4f25c0629b5573b1c1e63f4f
Brian Abedi <babedi3@gatech.edu>
2020-12-02 23:49:06 -0500

    Initial template

---
+++ InventoryManagement.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InventoryManagement.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ InventoryManagement.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InventoryManagement.Services
+{
+    public class Class1
+    {
+    }
+}
